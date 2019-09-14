7595d129b70e8f59bcdc53d21fda4b0946085504
Marius Scholtz <mc.scholtz@gmail.com>
2019-09-13 19:01:05 -0700

    fixed tests

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#clean top level
 rm CMakeCache.txt > /dev/null 2>&1
 rm Makefile > /dev/null 2>&1
 rm cmake_install.cmake > /dev/null 2>&1
@@ -6,8 +8,7 @@ rm CTestTestfile.cmake > /dev/null 2>&1
 rm -rf build > /dev/null 2>&1
 rm -rf CMakeFiles > /dev/null 2>&1
 
-
-rm tests/Makefile > /dev/null 2>&1
-rm tests/cmake_install.cmake > /dev/null 2>&1
-rm tests/CTestTestfile.cmake > /dev/null 2>&1
-rm -rf tests/CMakeFiles > /dev/null 2>&1
+#clean hello
+rm src/CTestTestfile.cmake > /dev/null 2>&1
+rm src/Makefile > /dev/null 2>&1
+rm -rf src/CMakeFiles > /dev/null 2>&1
